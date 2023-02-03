ed2988ed8b9d4d3b96dc4451a15458a7e1284707
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-04 01:03:08 +0200

    Task 15/48

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-// Завдання 13/48
-// Функція changeEven(numbers, value) приймає масив чисел numbers і оновлює кожен елемент, значення якого - це парне число, додаючи до нього значення параметра value.
+// Завдання 15/48
+// Використовуючи метод map(), зроби так, щоб у змінній titles вийшов масив назв книг (властивість title) з усіх об'єктів масиву books.
 
-// Виконай рефакторинг функції таким чином, щоб вона стала чистою - не змінювала масив чисел numbers, а створювала, наповнювала і повертала новий масив з оновленими значеннями.
-
-// Оголошена функція changeEven(numbers, value)
-// Функція changeEven не змінює значення параметра numbers
-// Виклик changeEven([1, 2, 3, 4, 5], 10) повертає новий масив [1, 12, 3, 14, 5]
-// Виклик changeEven([2, 8, 3, 7, 4, 6], 10) повертає новий масив [12, 18, 3, 7, 14, 16]
-// Виклик changeEven([17, 24, 68, 31, 42], 100) повертає новий масив [17, 124, 168, 31, 142]
-// Виклик changeEven([44, 13, 81, 92, 36, 54], 100) повертає новий масив [144, 13, 81, 192, 136, 154]
-// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+// Оголошена змінна books
+// Значення змінної books - це масив
+// Оголошена змінна titles
+// Значення змінної titles - це масив ["The Last Kingdom", "Beside Still Waters", "The Dream of a Ridiculous Man", "Redder Than Blood", "Enemy of God"]
+// Для перебирання масиву books використовується метод map() як чиста функція
 
 //Задача
 
-function changeEven(numbers, value) {
-  // Change code below this line
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] % 2 === 0) {
-      numbers[i] = numbers[i] + value;
-    }
-  }
-  // Change code above this line
-}
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+// Change code below this line
+
+const titles = books;
 
 //Решение
 
-function changeEven(numbers, value) {
-  return numbers.map((number) => (number % 2 === 0 ? number + value : number));
-}
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+// Change code below this line
+
+const titles = books.map((book) => book.title);
