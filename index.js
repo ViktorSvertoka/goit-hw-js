2af9df5f03f6b9e1c6c80707abde3932a093d329
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-03 23:44:19 +0200

    Task 10/48

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+// Завдання 10/48
+// Виконай рефакторинг функції calculateTotalPrice(orderedItems), замінивши її оголошення на стрілочну функцію. Заміни колбек-функцію, передану в метод forEach(), на стрілочну функцію.
 
+// Оголошена змінна calculateTotalPrice
+// Змінній calculateTotalPrice присвоєна стрілочна функція з параметром (orderedItems)
+// Для перебирання масиву orderedItems використаний метод forEach
+// Колбек для методу forEach - це стрілочна функція
+// Виклик функції calculateTotalPrice([12, 85, 37, 4]) повертає 138
+// Виклик функції calculateTotalPrice([164, 48, 291]) повертає 503
+// Виклик функції calculateTotalPrice([412, 371, 94, 63, 176]) повертає 1116
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+//Задача
+
+// Change code below this line
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+
+  orderedItems.forEach(function (item) {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+}
+// Change code above this line
+
+//Решение
+
+const calculateTotalPrice = (orderedItems) => {
+  let totalPrice = 0;
+  orderedItems.forEach((item) => {
+    totalPrice += item;
+  });
+  return totalPrice;
+};
