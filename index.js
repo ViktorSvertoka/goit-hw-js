b518cc4643556d856d7595c93e063d993d001f83
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-04 23:52:45 +0200

    Task 18/48
    Co-Authored-By: Сергій Євчишен <116813260+Sergiy-Yevchihen@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-// Доповни функцію getUserNames(users) таким чином, щоб вона повертала масив імен користувачів (властивість name) з масиву об'єктів в параметрі users.
+// Завдання 18/48
+// Доповни функцію getUserEmails(users) таким чином, щоб вона повертала масив поштових адрес користувачів (властивість email) з масиву об'єктів в параметрі users.
 
 // Оголошена змінна getUserNames
-// Змінній getUserNames присвоєна стрілочна функція з параметром (users).
+// Змінній getUserNames присвоєна стрілочна функція з параметром (users)
 // Для перебирання параметра users використовується метод map()
-// Виклик функції із зазначеним масивом користувачів повертає масив ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+// Виклик функції із зазначеним масивом користувачів повертає масив ["moorehensley@indexia.com", "sharlenebush@tubesys.com", "rossvazquez@xinware.com", "elmahead@omatom.com", "careybarr@nurali.com", "blackburndotson@furnigeer.com", "shereeanthony@kog.com"]
 // Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
 
 //Задача
 
 // Change code below this line
-const getUserNames = (users) => {};
+const getUserEmails = (users) => {};
 // Change code above this line
 
 //Решение
 
-const getUserNames = (users) => users.map((user) => user.name);
+const getUserEmails = (users) => users.map((user) => user.email);
