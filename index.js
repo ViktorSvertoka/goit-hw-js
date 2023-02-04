f1a0257d2f0de519888d70e5d6a508d0e2712d85
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-05 00:55:44 +0200

    Task 21/48
    Co-Authored-By: Сергій Євчишен <116813260+Sergiy-Yevchihen@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// Доповни код таким чином, щоб у змінній allGenres був масив всіх жанрів книг (властивість genres) з масиву books, а у змінній uniqueGenres - масив унікальних жанрів, без повторень.
+// Завдання 21/48
+// Використовуючи метод filter(), доповни код таким чином, щоб:
 
+// У змінній topRatedBooks утворився масив книг, рейтинг яких (властивість rating) більший за або дорівнює значенню змінної MIN_RATING.
+// У змінній booksByAuthor утворився масив книг, написаних автором з ім'ям (властивість author), яке збігається зі значенням у змінній AUTHOR.
 // Оголошена змінна books
 // Значення змінної books - це масив об'єктів
-// Оголошена змінна allGenres
-// Значення змінної allGenres - це масив ["adventure", "history", "fiction", "mysticism", "horror", "mysticism", "adventure"]
-// Оголошена змінна uniqueGenres
-// Значення змінної uniqueGenres - це масив ["adventure", "history", "fiction", "mysticism", "horror"]
-// Для обчислення значення змінної allGenders використаний метод flatMap()
-// Для обчислення значення змінної uniqueGenres використаний метод filter()
+// Оголошена змінна MIN_RATING
+// Значення змінної MIN_RATING - це число 8
+// Оголошена змінна AUTHOR
+// Значення змінної AUTHOR - це рядок "Bernard Cornwell"
+// Оголошена змінна topRatedBooks
+// Значення змінної topRatedBooks - це масив книг з рейтингом, вищим за 8
+// Оголошена змінна booksByAuthor
+// Значення змінної booksByAuthor - це масив книг, автор яких "Bernard Cornwell"
+// Для перебирання масиву books використаний метод filter()
 
 //Задача
 
@@ -15,22 +21,28 @@ const books = [
   {
     title: "The Last Kingdom",
     author: "Bernard Cornwell",
-    genres: ["adventure", "history"],
+    rating: 8.38,
   },
   {
     title: "Beside Still Waters",
     author: "Robert Sheckley",
-    genres: ["fiction", "mysticism"],
+    rating: 8.51,
   },
   {
-    title: "Redder Than Blood",
-    author: "Tanith Lee",
-    genres: ["horror", "mysticism", "adventure"],
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
   },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
 ];
+
+const MIN_RATING = 8;
+const AUTHOR = "Bernard Cornwell";
 // Change code below this line
-const allGenres = books;
-const uniqueGenres = allGenres;
+
+const topRatedBooks = books;
+const booksByAuthor = books;
 
 // Решение
 
@@ -38,22 +50,25 @@ const books = [
   {
     title: "The Last Kingdom",
     author: "Bernard Cornwell",
-    genres: ["adventure", "history"],
+    rating: 8.38,
   },
   {
     title: "Beside Still Waters",
     author: "Robert Sheckley",
-    genres: ["fiction", "mysticism"],
+    rating: 8.51,
   },
   {
-    title: "Redder Than Blood",
-    author: "Tanith Lee",
-    genres: ["horror", "mysticism", "adventure"],
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
   },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
 ];
+
+const MIN_RATING = 8;
+const AUTHOR = "Bernard Cornwell";
 // Change code below this line
 
-const allGenres = books.flatMap((book) => book.genres);
-const uniqueGenres = allGenres.filter(
-  (genre, index, genres) => genres.indexOf(genre) === index
-);
+const topRatedBooks = books.filter((book) => book.rating >= MIN_RATING);
+const booksByAuthor = books.filter((book) => book.author === AUTHOR);
