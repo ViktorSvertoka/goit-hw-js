65722a11fcd724a142f43c005d5af322cbb97d86
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-05 01:35:02 +0200

    Task 23/48
    Co-Authored-By: Сергій Євчишен <116813260+Sergiy-Yevchihen@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-// Завдання 22/48
-// Доповни функцію getUsersWithEyeColor(users, color) таким чином, щоб вона повертала масив користувачів, у яких колір очей (властивість eyeColor) збігається зі значенням параметра color.
+// Завдання 23/48
+// Доповни функцію getUsersWithAge(users, minAge, maxAge) таким чином, щоб вона повертала масив користувачів, вік яких (властивість age) потрапляє у проміжок від minAge до maxAge.
 
-// Оголошена змінна getUsersWithEyeColor
-// Змінній getUsersWithEyeColor присвоєна стрілочна функція з параметрами (users, color)
+// Оголошена змінна getUsersWithAge
+// Змінній getUsersWithAge присвоєна стрілочна функція з параметрами (users, minAge, maxAge)
 // Для перебирання параметра users використовується метод filter()
-// Якщо значення параметра color - це "blue", функція повертає масив об'єктів користувачів з іменами Moore Hensley, Sharlene Bush і Carey Barr
-// Якщо значення параметра color - це "green", функція повертає масив об'єктів користувачів з іменами Ross Vazquez і Elma Head
-// Якщо значення параметра color - це "brown", функція повертає масив об'єктів користувачів з іменами Blackburn Dotson і Sheree Anthony
-// Якщо значення параметра color - це будь-який інший рядок, функція повертає порожній масив
+// Якщо значення параметрів minAge і maxAge дорівнюють 20 і 30 відповідно, функція повертає масив об'єктів користувачів з іменами Ross Vazquez, Elma Head і Carey Barr
+// Якщо значення параметрів minAge і maxAge дорівнюють 30 і 40 відповідно, функція повертає масив об'єктів користувачів з іменами Moore Hensley, Sharlene Bush, Blackburn Dotson, Sheree Anthony
+// Якщо значення параметрів minAge і maxAge дорівнюють 80 і 100 відповідно, функція повертає порожній масив
 // Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
 
 //Задача
 
 // Change code below this line
-const getUsersWithEyeColor = (users, color) => {};
+const getUsersWithAge = (users, minAge, maxAge) => {};
 // Change code above this line
 
 // Решение
 
-const getUsersWithEyeColor = (users, color) =>
-  users.filter((user) => user.eyeColor === color);
+const getUsersWithAge = (users, minAge, maxAge) =>
+  users.filter((user) => user.age >= minAge && user.age <= maxAge);
