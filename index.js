46ca95b08c634ab283b8d716ad0a344dacf2c20c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-05 01:17:17 +0200

    Task 22/48
    Co-Authored-By: Сергій Євчишен <116813260+Sergiy-Yevchihen@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,74 +1,22 @@
-// Завдання 21/48
-// Використовуючи метод filter(), доповни код таким чином, щоб:
+// Завдання 22/48
+// Доповни функцію getUsersWithEyeColor(users, color) таким чином, щоб вона повертала масив користувачів, у яких колір очей (властивість eyeColor) збігається зі значенням параметра color.
 
-// У змінній topRatedBooks утворився масив книг, рейтинг яких (властивість rating) більший за або дорівнює значенню змінної MIN_RATING.
-// У змінній booksByAuthor утворився масив книг, написаних автором з ім'ям (властивість author), яке збігається зі значенням у змінній AUTHOR.
-// Оголошена змінна books
-// Значення змінної books - це масив об'єктів
-// Оголошена змінна MIN_RATING
-// Значення змінної MIN_RATING - це число 8
-// Оголошена змінна AUTHOR
-// Значення змінної AUTHOR - це рядок "Bernard Cornwell"
-// Оголошена змінна topRatedBooks
-// Значення змінної topRatedBooks - це масив книг з рейтингом, вищим за 8
-// Оголошена змінна booksByAuthor
-// Значення змінної booksByAuthor - це масив книг, автор яких "Bernard Cornwell"
-// Для перебирання масиву books використаний метод filter()
+// Оголошена змінна getUsersWithEyeColor
+// Змінній getUsersWithEyeColor присвоєна стрілочна функція з параметрами (users, color)
+// Для перебирання параметра users використовується метод filter()
+// Якщо значення параметра color - це "blue", функція повертає масив об'єктів користувачів з іменами Moore Hensley, Sharlene Bush і Carey Barr
+// Якщо значення параметра color - це "green", функція повертає масив об'єктів користувачів з іменами Ross Vazquez і Elma Head
+// Якщо значення параметра color - це "brown", функція повертає масив об'єктів користувачів з іменами Blackburn Dotson і Sheree Anthony
+// Якщо значення параметра color - це будь-який інший рядок, функція повертає порожній масив
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
 
 //Задача
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-];
-
-const MIN_RATING = 8;
-const AUTHOR = "Bernard Cornwell";
 // Change code below this line
-
-const topRatedBooks = books;
-const booksByAuthor = books;
+const getUsersWithEyeColor = (users, color) => {};
+// Change code above this line
 
 // Решение
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-];
-
-const MIN_RATING = 8;
-const AUTHOR = "Bernard Cornwell";
-// Change code below this line
-
-const topRatedBooks = books.filter((book) => book.rating >= MIN_RATING);
-const booksByAuthor = books.filter((book) => book.author === AUTHOR);
+const getUsersWithEyeColor = (users, color) =>
+  users.filter((user) => user.eyeColor === color);
