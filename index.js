cdaba1474abd3d9d11c4f1f48b997bf06088d3b6
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-06 00:01:33 +0200

    Task 30/48

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-// Завдання 29/48
-// Доповни функцію getUserWithEmail(users, email) таким чином, щоб вона повертала об'єкт користувача, пошта якого (властивість email) збігається зі значенням параметра email.
+// Використовуючи метод every(), доповни код таким чином, щоб:
 
-// Оголошена функція getUserWithEmail(users, email)
-
-// Для перебирання параметра users використовується метод find()
-
-// Якщо значення параметра email - це "shereeanthony@kog.com", функція повертає об'єкт користувача з ім'ям Sheree Anthony
-
-// Якщо значення параметра email - це "elmahead@omatom.com", функція повертає об'єкт користувача з ім'ям Elma Head
-
-// Якщо значення параметра email - це "blackburndotson@furnigeer.com", функція повертає об'єкт користувача з ім'ям Blackburn Dotson
-
-// Якщо в масиві users відсутній користувач з поштою із параметра email, функція повертає undefined
-
-// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+// У змінній eachElementInFirstIsEven був результат перевірки всіх елементів масиву firstArray на парність.
+// У змінній eachElementInFirstIsOdd був результат перевірки всіх елементів масиву firstArray на непарність.
+// У змінній eachElementInSecondIsEven був результат перевірки всіх елементів масиву secondArray на парність.
+// У змінній eachElementInSecondIsOdd був результат перевірки всіх елементів масиву secondArray на непарність.
+// У змінній eachElementInThirdIsEven був результат перевірки всіх елементів масиву thirdArray на парність.
+// У змінній eachElementInThirdIsOdd був результат перевірки всіх елементів масиву thirdArray на непарність.
+// Оголошена змінна firstArray
+// Значення змінної firstArray - це масив [26, 94, 36, 18]
+// Оголошена змінна secondArray
+// Значення змінної secondArray - це масив [17, 61, 23]
+// Оголошена змінна thirdArray
+// Значення змінної thirdArray - це масив [17, 26, 94, 61, 36, 23, 18]
+// Оголошена змінна eachElementInFirstIsEven
+// Значення змінної eachElementInFirstIsEven - це буль true
+// Оголошена змінна eachElementInFirstIsOdd
+// Значення змінної eachElementInFirstIsOdd - це буль false
+// Оголошена змінна eachElementInSecondIsEven
+// Значення змінної eachElementInSecondIsEven - це буль false
+// Оголошена змінна eachElementInSecondIsOdd
+// Значення змінної eachElementInSecondIsOdd - це буль true
+// Оголошена змінна eachElementInThirdIsEven
+// Значення змінної eachElementInThirdIsEven - це буль false
+// Оголошена змінна eachElementInThirdIsOdd
+// Значення змінної eachElementInThirdIsOdd - це буль false
+// Для перебирання масивів використаний метод every()
 
 //Задача
 
-const getUserWithEmail = (users, email) => {};
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change code below this line
+
+const eachElementInFirstIsEven = firstArray;
+const eachElementInFirstIsOdd = firstArray;
+
+const eachElementInSecondIsEven = secondArray;
+const eachElementInSecondIsOdd = secondArray;
+
+const eachElementInThirdIsEven = thirdArray;
+const eachElementInThirdIsOdd = thirdArray;
 
 //Решение
 
-const getUserWithEmail = (users, email) => {
-  return users.find((user) => user.email === email);
-};
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change code below this line
+
+const eachElementInFirstIsEven = firstArray.every((num) => num % 2 === 0);
+const eachElementInFirstIsOdd = firstArray.every((num) => num % 2 !== 0);
+
+const eachElementInSecondIsEven = secondArray.every((num) => num % 2 === 0);
+const eachElementInSecondIsOdd = secondArray.every((num) => num % 2 !== 0);
+
+const eachElementInThirdIsEven = thirdArray.every((num) => num % 2 === 0);
+const eachElementInThirdIsOdd = thirdArray.every((num) => num % 2 !== 0);
