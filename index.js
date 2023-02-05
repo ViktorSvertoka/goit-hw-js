60af47edd6f66b7304d0a4dbf2c6d39f4a4c43c3
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-05 21:12:13 +0200

    Task 26/48
    Co-Authored-By: Сергій Євчишен <116813260+Sergiy-Yevchihen@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-// Завдання 25/48
-// Доповни функцію getFriends(users) таким чином, щоб вона повертала масив друзів всіх користувачів (властивість friends). У декількох користувачів можуть бути однакові друзі, зроби так, щоб масив, що повертається, не містив повторень.
+// Доповни функцію getActiveUsers(users) таким чином, щоб вона повертала масив активних користувачів, значення властивості isActive яких - true.
 
-// Оголошена змінна getFriends
-// Змінній getFriends присвоєна стрілочна функція з параметром (users)
-// Виклик функції із зазначеним масивом користувачів повертає масив ["Sharron Pace", "Briana Decker", "Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner", "Goldie Gentry", "Aisha Tran", "Jordan Sampson", "Eddie Strong", "Jacklyn Lucas", "Linda Chapman"]
+// Оголошена змінна getActiveUsers Змінній getActiveUsers присвоєна стрілочна функція з параметром users
+// Для перебирання параметра users використовується метод filter()
+// Виклик функції із зазначеним масивом користувачів повертає масив об'єктів користувачів з іменами Sharlene Bush, Elma Head, Carey Barr і Sheree Anthony
 // Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
 
 //Задача
 
-const getFriends = (users) => {};
+const getActiveUsers = (users) => {};
 
 //Решение
 
-const getFriends = (users) => {
-  let friends = [];
-  users.forEach((user) => {
-    user.friends.forEach((friend) => {
-      if (!friends.includes(friend)) {
-        friends.push(friend);
-      }
-    });
-  });
-  return friends;
+const getActiveUsers = (users) => {
+  return users.filter((user) => user.isActive);
 };
