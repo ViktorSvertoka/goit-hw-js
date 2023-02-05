979b7264bc147f3e87377b2b3a59671d50ecb334
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-06 01:16:56 +0200

    Task 36/48

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-// Завдання 35/48
-// Нашому сервісу необхідно розрахувати середній час, проведений в одній грі для кожного гравця, і отримати загальну суму цих значень часу. Розрахувати час для кожного з гравців можна, розділивши його час (властивість playtime) на кількість ігор (властивість gamesPlayed).
+// Завдання 36/48
+// Доповни функцію calculateTotalBalance(users) таким чином, щоб вона рахувала і повертала суму всіх коштів (властивість balance), які зберігають користувачі з масиву users.
 
-// Оголошена змінна players
-// Значення змінної players - це масив об'єктів гравців
-// Оголошена змінна totalAveragePlaytimePerGame
-// Значення змінної totalAveragePlaytimePerGame - це число 1023
-// Для перебирання масиву players використовується метод reduce()
+// Оголошена змінна calculateTotalBalance
+// Змінній calculateTotalBalance присвоєна стрілочна функція з параметром (users)
+// Для перебирання параметра users використовується метод reduce()
+// Виклик функції із зазначеним масивом користувачів повертає число 20916
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
 
 //Задача
 
-const players = [
-  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-  { name: "Poly", playtime: 469, gamesPlayed: 2 },
-  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-];
-// Change code below this line
-
-const totalAveragePlaytimePerGame = players;
+const calculateTotalBalance = (users) => {};
 
 //Решение
 
-const players = [
-  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-  { name: "Poly", playtime: 469, gamesPlayed: 2 },
-  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-];
-// Change code below this line
-
-const totalAveragePlaytimePerGame = players.reduce(
-  (totalTime, player) => totalTime + player.playtime / player.gamesPlayed,
-  0
-);
+const calculateTotalBalance = (users) =>
+  users.reduce((total, user) => total + user.balance, 0);
