d1b3fa67a1d4b90f51d4b1005e67d6c0eebe2382
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-06 00:23:03 +0200

    Task 32/48

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
-// Завдання 31/48
-// Доповни функцію isEveryUserActive(users) таким чином, щоб вона перевіряла, чи всі користувачі зараз активні (властивість isActive) і повертала true або false.
+// Використовуючи метод some(), доповни код таким чином, щоб:
 
-// Оголошена змінна isEveryUserActive
-// Змінній isEveryUserActive присвоєна стрілочна функція з параметром (users)
-// Для перебирання параметра users використовується метод every()
-// Виклик функції із зазначеним масивом користувачів повертає false
-// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+// У змінній anyElementInFirstIsEven був результат перевірки наявності парних елементів в масиві firstArray.
+// У змінній anyElementInFirstIsOdd був результат перевірки наявності непарних елементів в масиві firstArray.
+// У змінній anyElementInSecondIsEven був результат перевірки наявності парних елементів в масиві secondArray.
+// У змінній anyElementInSecondIsOdd був результат перевірки наявності непарних елементів в масиві secondArray.
+// У змінній anyElementInThirdIsEven був результат перевірки наявності парних елементів в масиві thirdArray.
+// У змінній anyElementInThirdIsOdd був результат перевірки наявності непарних елементів в масиві thirdArray.
+// Оголошена змінна firstArray
+// Значення змінної firstArray - це масив [26, 94, 36, 18]
+// Оголошена змінна secondArray
+// Значення змінної secondArray - це масив [17, 61, 23]
+// Оголошена змінна thirdArray
+// Значення змінної thirdArray - це масив [17, 26, 94, 61, 36, 23, 18]
+// Оголошена змінна anyElementInFirstIsEven
+// Значення змінної anyElementInFirstIsEven - це буль true
+// Оголошена змінна anyElementInFirstIsOdd
+// Значення змінної anyElementInFirstIsOdd - це буль false
+// Оголошена змінна anyElementInSecondIsEven
+// Значення змінної anyElementInSecondIsEven - це буль false
+// Оголошена змінна anyElementInSecondIsOdd
+// Значення змінної anyElementInSecondIsOdd - це буль true
+// Оголошена змінна anyElementInThirdIsEven
+// Значення змінної anyElementInThirdIsEven - це буль true
+// Оголошена змінна anyElementInThirdIsOdd
+// Значення змінної anyElementInThirdIsOdd - це буль true
+// Для перебирання масивів використаний метод some()
 
 //Задача
 
-// Change code below this line
-const isEveryUserActive = (users) => {};
-// Change code above this line
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change below this line
+
+const anyElementInFirstIsEven = firstArray;
+const anyElementInFirstIsOdd = firstArray;
+
+const anyElementInSecondIsEven = secondArray;
+const anyElementInSecondIsOdd = secondArray;
+
+const anyElementInThirdIsEven = thirdArray;
+const anyElementInThirdIsOdd = thirdArray;
 
 //Решение
 
-// Change code below this line
-const isEveryUserActive = (users) => users.every((user) => user.isActive);
-// Change code above this line
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change below this line
+
+const anyElementInFirstIsEven = firstArray.some((num) => num % 2 === 0);
+const anyElementInFirstIsOdd = firstArray.some((num) => num % 2 !== 0);
+
+const anyElementInSecondIsEven = secondArray.some((num) => num % 2 === 0);
+const anyElementInSecondIsOdd = secondArray.some((num) => num % 2 !== 0);
+
+const anyElementInThirdIsEven = thirdArray.some((num) => num % 2 === 0);
+const anyElementInThirdIsOdd = thirdArray.some((num) => num % 2 !== 0);
