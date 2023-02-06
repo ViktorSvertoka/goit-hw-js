b9ed01faf6ca492a9065c57c3c7112f39cb9c54b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-07 00:51:59 +0200

    Autocheck module 5 - task 8/20

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// Завдання 7/20
-// Додай класу Car метод constructor, який приймає три параметри:
-
-// brand - марка автомобіля.
-// model - модель автомобіля.
-// price - ціна автомобіля.
-// Клас Car повинен створювати об'єкт з однойменними властивостями brand, model і price, значеннями яких повинні бути передані аргументи під час його виклику з оператором new.
+// Завдання 8/20
+// Виконай рефакторинг класу Car таким чином, щоб він приймав один параметр - об'єкт з властивостями brand, model і price. Деструктуризуй об'єкт в сигнатурі (підписі) конструктора.
 
 // Оголошений клас Car
 // Клас Car має метод constructor
-// В результаті виклику new Car("Audi", "Q3", 36000) утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
-// В результаті виклику new Car("BMW", "X5", 58900) утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
-// В результаті виклику new Car("Nissan","Murano", 31700) утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
+// В результаті виклику new Car({ brand: "Audi", model: "Q3", price: 36000 }) утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
+// В результаті виклику new Car({ brand: "BMW", model: "X5", price: 58900 }) утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
+// В результаті виклику new Car({ brand: "Nissan", model: "Murano", price: 31700 })
 
 // Задача
 
 class Car {
   // Change code below this line
+  constructor(brand, model, price) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
   // Change code above this line
 }
 
@@ -23,8 +23,7 @@ class Car {
 
 class Car {
   // Change code below this line
-  constructor(brand, model, price) {
-    // Ініціалізація властивостей екземпляра
+  constructor({ brand, model, price }) {
     this.brand = brand;
     this.model = model;
     this.price = price;
