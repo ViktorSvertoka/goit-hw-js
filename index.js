74e4e453543a9ab3cf2362b30c2360c749635a34
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-08 00:56:42 +0200

    Autocheck module 5 - task 14/20

--- index.js
+++ index.js
@@ -1,76 +1,103 @@
-// Завдання 13/20
-// Виконай рефакторинг класу Storage, зробивши властивість items приватною.
+// Завдання 14/20
+// Виконай рефакторинг класу StringBuilder, зробивши властивість value приватною.
 
 // Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
 
-// Оголошений клас Storage
-// Об'єкт storage не містить властивості items
-// В класі Storage оголошений метод getItems
-// В класі Storage оголошений метод addItem
-// В класі Storage оголошений метод removeItem
-// Властивість items в класі Storage оголошена приватною
-// Конструктор класу приймає властивість items
-// В результаті виклику new Storage(["Nanitoids", "Prolonger", "Antigravitator"]) значення змінної storage - це об'єкт
-// Перший виклик storage.getItems(), відразу після ініціалізації екземпляра, повертає масив ["Nanitoids", "Prolonger", "Antigravitator"]
-// Другий виклик, storage.getItems(), після виклику storage.addItem("Droid"), повертає масив ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-// Третій виклик storage.getItems(), після виклику storage.removeItem("Prolonger"), повертає масив ["Nanitoids", "Antigravitator", "Droid"]
+// Оголошений клас StringBuilder
+
+// Властивість value в класі StringBuilder оголошена приватною
+
+// В класі StringBuilder оголошений метод getValue
+
+// В класі StringBuilder оголошений метод padEnd
+
+// В класі StringBuilder оголошений метод padStart
+
+// В класі StringBuilder оголошений метод padBoth
+
+// В результаті виклику new StringBuilder('.') значення змінної builder - це об'єкт
+
+// Об'єкт builder не містить властивості value
+
+// Перший виклик builder.getValue(), відразу після ініціалізації екземпляра, повертає рядок .
+
+// Другий виклик builder.getValue(), після виклику builder.padStart("^"), повертає рядок ^.
+
+// Третій виклик builder.getValue(), після виклику builder.padEnd("^"), повертає рядок ^.^
+
+// Четвертий виклик builder.getValue(), після виклику builder.padBoth("="), повертає рядок =^.^=
 
 // Задача
 
-class Storage {
+class StringBuilder {
   // Change code below this line
-  constructor(items) {
-    this.items = items;
+
+  constructor(initialValue) {
+    this.value = initialValue;
   }
 
-  getItems() {
-    return this.items;
+  getValue() {
+    return this.value;
   }
 
-  addItem(newItem) {
-    this.items.push(newItem);
+  padEnd(str) {
+    this.value += str;
   }
 
-  removeItem(itemToRemove) {
-    this.items = this.items.filter((item) => item !== itemToRemove);
+  padStart(str) {
+    this.value = str + this.value;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
   }
 }
 
 // Change code above this line
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
 
 // Решение
 
-class Storage {
+class StringBuilder {
   // Change code below this line
-  #items;
+  #value;
 
-  constructor(items) {
-    this.#items = items;
+  constructor(initialValue) {
+    this.#value = initialValue;
   }
 
-  getItems() {
-    return this.#items;
+  getValue() {
+    return this.#value;
   }
 
-  addItem(newItem) {
-    this.#items.push(newItem);
+  padEnd(str) {
+    this.#value += str;
   }
 
-  removeItem(itemToRemove) {
-    this.#items = this.#items.filter((item) => item !== itemToRemove);
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
   }
 }
 
 // Change code above this line
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
