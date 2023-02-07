d0ce2e1ad6c035ae5937e1265a1b92281417f85b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-07 23:59:36 +0200

    Autocheck module 5 - task 11/20

--- index.js
+++ index.js
@@ -1,56 +1,71 @@
-// Завдання 10/20
-// Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. Клас очікує тільки один аргумент - початковий масив товарів, який записується у властивість items об'єкта, що створюється.
+// Завдання 11/20
+// Напиши клас StringBuilder, який приймає один параметр initialValue - довільний рядок, який записується у властивість value об'єкта, що створюється.
 
 // Оголоси наступні методи класу:
 
-// getItems() - повертає масив поточних товарів у властивості items об'єкта, який викликає цей метод.
-// addItem(newItem) - приймає новий товар newItem і додає його в масив товарів у властивості items об'єкта, який викликає цей метод.
-// removeItem(itemToRemove) - приймає товар itemToRemove і видаляє його з масиву товарів у властивості items об'єкта, який викликає цей метод.
+// getValue() - повертає поточне значення властивості value.
+// padEnd(str) - отримує параметр str (рядок) і додає його в кінець значення властивості value об'єкта, який викликає цей метод.
+// padStart(str) - отримує параметр str (рядок) і додає його на початок значення властивості value об'єкта, який викликає цей метод.
+// padBoth(str) - отримує параметр str (рядок) і додає його на початок і в кінець значення властивості value об'єкта, який викликає цей метод.
 // Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
 
-// Оголошений клас Storage
-// В класі Storage оголошений метод getItems
-// В класі Storage оголошений метод addItem
-// В класі Storage оголошений метод removeItem
-// Метод getItems повертає значення властивості items екземпляра класу, який його викликає
-// Метод addItem змінює властивість items екземпляра класу, який його викликає
-// Метод removeItem змінює властивість items екземпляра класу, який його викликає
-// В результаті виклику new Storage(["Nanitoids", "Prolonger", "Antigravitator"]) значення змінної storage - це об'єкт
-// Об'єкт storage містить властивість items
-// Перший виклик storage.getItems(), відразу після інціалізаціі екземпляра, повертає масив ["Nanitoids", "Prolonger", "Antigravitator"]
-// Другий виклик, storage.getItems(), після виклику storage.addItem("Droid"), повертає масив ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-// Третій виклик storage.getItems(), після виклику storage.removeItem("Prolonger"), повертає масив ["Nanitoids", "Antigravitator", "Droid"]
+// Оголошений клас StringBuilder
+// В класі StringBuilder оголошений метод getValue
+// Метод getValue повертає значення властивості value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padEnd
+// Метод padEnd змінює властивість value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padStart
+// Метод padStart змінює властивість value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padBoth
+// Метод padBoth змінює властивість value екземпляра класу, який його викликає
+// В результаті виклику new StringBuilder(".") значення змінної builder - це об'єкт
+// Об'єкт builder містить властивість value
+// Перший виклик builder.getValue(), відразу після ініціалізації екземпляра, повертає рядок .
+// Другий виклик builder.getValue(), після виклику builder.padStart("^"), повертає рядок ^.
+// Третій виклик builder.getValue(), після виклику builder.padEnd("^"), повертає рядок ^.^
+// Четвертий виклик builder.getValue(), після виклику builder.padBoth("="), повертає рядок =^.^=
 
 // Задача
 
 // Change code above this line
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
 
 // Решение
 
-class Storage {
-  constructor(items) {
-    this.items = items;
+class StringBuilder {
+  constructor(str) {
+    this.value = str;
   }
-  getItems() {
-    return this.items;
+  getValue() {
+    return this.value;
   }
-  addItem(newItem) {
-    this.items.push(newItem);
+  padEnd(str) {
+    this.value = this.value + str;
+    return this.value;
   }
-  removeItem(itemToRemove) {
-    this.items = this.items.filter((item) => item !== itemToRemove);
+  padStart(str) {
+    this.value = str + this.value;
+    return this.value;
+  }
+  padBoth(str) {
+    this.value = str + this.value + str;
+    return this.value;
   }
 }
+
 // Change code above this line
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
