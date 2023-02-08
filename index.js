fd73b36b703202d04d4241f51d40a1f3bf9198ad
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-08 02:39:47 +0200

    Autocheck module 5 - task 17/20

--- index.js
+++ index.js
@@ -1,81 +1,53 @@
-// Завдання 16/20
-// Виконай рефакторинг класу Car. Додай публічну статичну властивість MAX_PRICE зі значенням 50000 - максимально допустима ціна автомобіля.
+// Завдання 17/20
+// Додай класу Car публічний статичний метод checkPrice (price), що приймає ціну автомобіля. Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
 
-// Додай сетеру price перевірку значення параметра newPrice, що передається. Якщо воно більше за MAX_PRICE, сеттер нічого не робить, а якщо менше або дорівнює, то перезаписує ціну автомобіля.
+// Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок "Error! Price exceeds the maximum".
+// В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits".
+// Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів, щоб показати, як буде використовуватися метод checkPrice(price).
 
 // Оголошений клас Car
-// Клас Car містить статичну властивість MAX_PRICE
-// Значення статичної властивості MAX_PRICE - це число 50000
-// Екземпляр не містить властивості MAX_PRICE
-// В класі Car оголошений гетер price
-// В класі Car оголошений сетер price
-// Виклик сетера price в екземпляра класу, зі значенням аргументу меншим за значення MAX_PRICE, змінює властивість #price
-// Виклик сетера price в екземпляра класу, зі значенням аргументу більшим за значення MAX_PRICE, не змінює властивість #price
+// Клас Car містить статичний метод checkPrice(price)
+// Виклик Car.checkPrice(36000) повертає рядок "Success! Price is within acceptable limits"
+// Виклик Car.checkPrice(18000) повертає рядок "Success! Price is within acceptable limits"
+// Виклик Car.checkPrice(64000) повертає рядок "Error! Price exceeds the maximum"
+// Виклик Car.checkPrice(57000) повертає рядок "Error! Price exceeds the maximum"
 
 // Задача
 
 class Car {
+  static #MAX_PRICE = 50000;
   // Change code below this line
 
-  static MAX_PRICE = 50000;
-
-  #price;
-
-  constructor({ price }) {
-    this.#price = price;
-  }
-
-  get price() {
-    return this.#price;
-  }
-
-  set price(newPrice) {
-    if (newPrice <= Car.MAX_PRICE) {
-      this.#price = newPrice;
-    }
-  }
   // Change code above this line
+  constructor({ price }) {
+    this.price = price;
+  }
 }
 
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
 
-audi.price = 49000;
-console.log(audi.price); // 49000
-
-audi.price = 51000;
-console.log(audi.price); // 49000
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
 
 // Решение
 
 class Car {
+  static #MAX_PRICE = 50000;
   // Change code below this line
 
-  static MAX_PRICE = 50000;
-
-  #price;
-
-  constructor({ price }) {
-    this.#price = price;
-  }
-
-  get price() {
-    return this.#price;
-  }
-
-  set price(newPrice) {
-    if (newPrice <= Car.MAX_PRICE) {
-      this.#price = newPrice;
+  // Change code above this line
+  static checkPrice(price) {
+    if (price > this.#MAX_PRICE) {
+      return "Error! Price exceeds the maximum";
+    } else {
+      return "Success! Price is within acceptable limits";
     }
   }
-  // Change code above this line
 }
 
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
 
-audi.price = 49000;
-console.log(audi.price); // 49000
-
-audi.price = 51000;
-console.log(audi.price); // 49000
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
