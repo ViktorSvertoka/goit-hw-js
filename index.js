241ae6b8dcfdae1ee64eb636481c0d6da5f74de6
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-08 13:20:57 +0200

    Autocheck module 5 - task 18/20

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
-// Завдання 17/20
-// Додай класу Car публічний статичний метод checkPrice (price), що приймає ціну автомобіля. Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
+// Завдання 18/20
+// У застосунку потрібен адміністратор з можливістю додавати пошти користувачів у чорний список.
 
-// Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок "Error! Price exceeds the maximum".
-// В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits".
-// Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів, щоб показати, як буде використовуватися метод checkPrice(price).
-
-// Оголошений клас Car
-// Клас Car містить статичний метод checkPrice(price)
-// Виклик Car.checkPrice(36000) повертає рядок "Success! Price is within acceptable limits"
-// Виклик Car.checkPrice(18000) повертає рядок "Success! Price is within acceptable limits"
-// Виклик Car.checkPrice(64000) повертає рядок "Error! Price exceeds the maximum"
-// Виклик Car.checkPrice(57000) повертає рядок "Error! Price exceeds the maximum"
+// Оголоси клас Admin, який наслідує від класу User
+// Додай класу Admin публічну статичну властивість AccessLevel (рівень доступу), значення якої — це об'єкт {BASIC: "basic", SUPERUSER: "superuser"}
+// Оголошений клас Admin
+// Клас Admin наслідує від класу User
+// Клас Admin містить публічну статичну властивість AccessLevel
+// Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
+// Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
 
 // Задача
 
-class Car {
-  static #MAX_PRICE = 50000;
-  // Change code below this line
+class User {
+  constructor(email) {
+    this.email = email;
+  }
 
-  // Change code above this line
-  constructor({ price }) {
-    this.price = price;
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
   }
 }
-
-const audi = new Car({ price: 36000 });
-const bmw = new Car({ price: 64000 });
-
-console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
-console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+// Change code below this line
 
 // Решение
 
-class Car {
-  static #MAX_PRICE = 50000;
-  // Change code below this line
+class User {
+  constructor(email) {
+    this.email = email;
+  }
 
-  // Change code above this line
-  static checkPrice(price) {
-    if (price > this.#MAX_PRICE) {
-      return "Error! Price exceeds the maximum";
-    } else {
-      return "Success! Price is within acceptable limits";
-    }
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
   }
 }
+// Change code below this line
 
-const audi = new Car({ price: 36000 });
-const bmw = new Car({ price: 64000 });
-
-console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
-console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+class Admin extends User {
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+}
