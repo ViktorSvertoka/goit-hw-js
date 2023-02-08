949da8d22bb39fc8ada1546fdff7d16603502ac5
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-08 13:38:31 +0200

    Autocheck module 5 - task 19/20

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-// Завдання 18/20
-// У застосунку потрібен адміністратор з можливістю додавати пошти користувачів у чорний список.
+// Завдання 19/20
+// Додай класу Admin метод constructor, який приймає один параметр - об'єкт налаштувань з двома властивостями email і accessLevel. Додай класу Admin публічну властивість accessLevel, значення якої буде передаватися під час виклику конструктора.
 
-// Оголоси клас Admin, який наслідує від класу User
-// Додай класу Admin публічну статичну властивість AccessLevel (рівень доступу), значення якої — це об'єкт {BASIC: "basic", SUPERUSER: "superuser"}
+// Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію екземпляра під оголошенням класу.
+
 // Оголошений клас Admin
 // Клас Admin наслідує від класу User
 // Клас Admin містить публічну статичну властивість AccessLevel
+// Клас Admin містить метод constructor з параметром у вигляді об'єкта {email, accessLevel}
+// В класі Admin в конструкторі для властивості email використовується звернення до конструктора батьківського класу
 // Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
 // Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
 
 // Задача
 
 class User {
+  email;
+
   constructor(email) {
     this.email = email;
   }
@@ -24,28 +28,64 @@ class User {
     this.email = newEmail;
   }
 }
-// Change code below this line
-
-// Решение
-
-class User {
-  constructor(email) {
-    this.email = email;
-  }
-
-  get email() {
-    return this.email;
-  }
-
-  set email(newEmail) {
-    this.email = newEmail;
-  }
-}
-// Change code below this line
 
 class Admin extends User {
+  // Change code below this line
+
   static AccessLevel = {
     BASIC: "basic",
     SUPERUSER: "superuser",
   };
+
+  // Change code above this line
 }
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+// Решение
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  // Change code below this line
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+
+  // Change code above this line
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
